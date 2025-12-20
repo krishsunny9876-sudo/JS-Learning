03210f0933ecda8a93b9fcdff9c337554a923ec7
krishsunny9876-sudo <krishsunny9876@gmail.com>
2025-12-20 18:20:19 +0000

    ch1 complete

--- LearnJS/ch1.js
+++ LearnJS/ch1.js
@@ -1,2 +1,9 @@
 let username;
-username
+username = "   kIrteN_124  "//assuming that it is given by any user
+let a = username.trim()
+if ((a.length) < 7) {
+    console.log("Username is Invalid due to shorter length!!")
+    }
+    else {
+        console.log(`Username : ${a.toLowerCase()}`)
+        }
