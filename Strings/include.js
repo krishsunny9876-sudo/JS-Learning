311195ebb5715602ba7877659944d1493074e0a2
krishsunny9876-sudo <krishsunny9876@gmail.com>
2025-12-20 19:32:03 +0000

    stringss

---
+++ Strings/include.js
@@ -0,0 +1,8 @@
+let a = "kirten"
+
+if(a.includes("k")){
+console.log(true);
+}
+else{
+    console.log(false)
+}
---
+++ Strings/toarray.js
@@ -0,0 +1,21 @@
+let str = "Hello";
+let arr = str.split("");
+
+console.log(arr);
+// ["H", "e", "l", "l", "o"] 
+
+//OR
+
+let str1 = "Hello world this is JS";
+let arr1 = str.split(" ");
+
+console.log(arr);
+// ["Hello", "world", "this", "is", "JS"]
+
+//OR
+
+let str2 = "apple,banana,orange";
+let arr2 = str.split(",");
+
+console.log(arr);
+// ["apple", "banana", "orange"]
