71c6dcb0dadf741ab590789ae5572dc229b5c041
krishsunny9876-sudo <krishsunny9876@gmail.com>
2025-12-23 14:23:49 +0000

    more obj and array

--- Objects/assign.js
+++ Objects/assign.js
@@ -1,11 +1,8 @@
-let obj1 = { "user1": "kirten", "user2": "boby" }
-let obj2 = { "user3": "dipanshu", "user4": "sunny" }
-let obj3 = { "user5": "raju", "usre6": "vijay" }
+let obj1 ={"name":"kirten","roll no.":46}
+let obj2={"id":23,"address":"xxyy"}
 
-Object.assign(obj1, obj2, obj3)//it will add all values of obj2 and obj 3 in obj1
+let a = Object.assign({},obj1,obj2)
+let b = {...obj1,...obj2}
 
-let obj4 = Object.assign({},obj1,obj2,obj3)//it will add all value of obj1,obj2 and obj3
-// in {}(empty object) and then it wil assign to obj4
-
-console.log(obj1);
-console.log(obj4);
+console.log(a)
+console.log(b)
